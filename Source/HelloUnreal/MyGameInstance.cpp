771e7dae4e1451cb5475dc073b62d8bc60558ef8
son_sehyup <tpguq1104@naver.com>
2025-08-19 01:55:45 +0900

    add test

--- Source/HelloUnreal/MyGameInstance.cpp
+++ Source/HelloUnreal/MyGameInstance.cpp
@@ -8,4 +8,5 @@ void UMyGameInstance::Init()
 	Super::Init(); // 언리얼 엔진에서의 기본이다. 언리얼 엔진의 base class의 Init() 함수를 오버라이드하여 호출한다.
 
 	UE_LOG(LogTemp, Log, TEXT("%s"), TEXT("Hello Unreal!"));
+	UE_LOG(LogTemp, Log, TEXT("%s"), TEXT("Hello Unreal!2"));
 }
